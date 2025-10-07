523c102367242948e8292c5c5551d970b8807f1d
I758995 <peter.barkanyi@sap.com>
2025-10-07 21:59:14 +0200

    eventcontext

---
+++ srv/src/main/java/szakdolgozat/project_tracking/utilities/EventContextAnalyzer.java
@@ -0,0 +1,117 @@
+package szakdolgozat.project_tracking.utilities;
+
+import com.sap.cds.ql.cqn.AnalysisResult;
+import com.sap.cds.ql.cqn.CqnAnalyzer;
+import com.sap.cds.ql.cqn.CqnStructuredTypeRef;
+import com.sap.cds.reflect.CdsEntity;
+import com.sap.cds.services.EventContext;
+import com.sap.cds.services.cds.CdsDeleteEventContext;
+import com.sap.cds.services.cds.CdsReadEventContext;
+import com.sap.cds.services.draft.DraftCancelEventContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
+
+@Component
+public class EventContextAnalyzer {
+    /**
+     * Get the target keys for a Delete event
+     *
+     * @param context The event context to analyze
+     * @return The targetKeys Map of the analysis result
+     */
+    public Map<String, Object> targetKeys(CdsDeleteEventContext context) {
+        return createAnalyzer(context).analyze(context.getCqn()).targetKeys();
+    }
+
+    /**
+     * Get the target keys for a DraftCancel event
+     *
+     * @param context The event context to analyze
+     * @return The targetKeys Map of the analysis result
+     */
+    public Map<String, Object> targetKeys(DraftCancelEventContext context) {
+        return targetKeys(context, ctx -> ctx.getCqn().ref());
+    }
+
+    /**
+     * Get the target keys for a Read event
+     *
+     * @param context The event context to analyze
+     * @return The targetKeys Map of the analysis result
+     */
+    public Map<String, Object> targetKeys(CdsReadEventContext context) {
+        return targetKeys(context, ctx -> ctx.getCqn().ref());
+    }
+
+    /**
+     * Creates a CqnAnalyzer for the given event context, analyzes the context with the created analyzer and returns the target keys.
+     * The purpose of this method is that it's much easier to mock than the underlying implementation (and of course reusability).
+     *
+     * @param eventContext The event context to analyze
+     * @param getCqnRef    Lambda to create a CqnStructuredTypeRef from the context. This is a parameter because there's no common ancestor that would enforce the .getCqn() method that is present on EventContext implementations
+     * @param <T>          Type of the event context
+     * @return The targetKeys Map of the analysis result
+     */
+    public <T extends EventContext> Map<String, Object> targetKeys(T eventContext, Function<T, ? extends CqnStructuredTypeRef> getCqnRef) {
+        return analyze(eventContext, getCqnRef).targetKeys();
+    }
+
+    /**
+     * For multi-level CQN queries this returns Maps with key-value pairs for each level of the query
+     * e.g. /SpecificationVersion(a=A,b=B)/propertyInstanceAssignments(a=A,c=C)/propertyInstance/attributePropertyRows would return [ {a:A, b:B}, {a:A, c:C}, {}, {} ]
+     *
+     * @param eventContext The event context to analyze
+     * @param getCqnRef    Lambda to create a CqnStructuredTypeRef from the context. This is a parameter because there's no common ancestor that would enforce the .getCqn() method that is present on EventContext implementations
+     * @param <T>          Type of the event context
+     * @return Segments corresponding to the levels of the CQN
+     */
+//    public <T extends EventContext> List<SegmentAnalysisResult> segments(T eventContext, Function<T, ? extends CqnStructuredTypeRef> getCqnRef) {
+//        return StreamSupport.stream(analyze(eventContext, getCqnRef).spliterator(), false)
+//                .map(SegmentAnalysisResult::fromResolvedSegment)
+//                .toList();
+//    }
+
+    /**
+     * Get the target values for a Delete event
+     *
+     * @param context The event context to analyze
+     * @return The targetValues Map of the analysis result
+     */
+    public Map<String, Object> targetValues(CdsDeleteEventContext context) {
+        return createAnalyzer(context).analyze(context.getCqn()).targetValues();
+    }
+
+    /**
+     * Get the target values for a Read event
+     *
+     * @param context The event context to analyze
+     * @return The targetValues Map of the analysis result
+     */
+    public Map<String, Object> targetValues(CdsReadEventContext context) {
+        return createAnalyzer(context).analyze(context.getCqn()).targetValues();
+    }
+
+    /**
+     * Creates a CqnAnalyzer for the given event context, analyzes the context with the created analyzer and returns the target entity.
+     *
+     * @param eventContext The event context to analyze
+     * @param getCqnRef Lambda to create a CqnStructuredTypeRef from the context. This is a parameter because there's no common ancestor that would enforce the .getCqn() method that is present on EventContext implementations
+     * @param <T> Type of the event context
+     * @return The target CdsEntity
+     */
+    public <T extends EventContext> CdsEntity targetEntity(T eventContext, Function<T, ? extends CqnStructuredTypeRef> getCqnRef) {
+        return analyze(eventContext, getCqnRef).targetEntity();
+    }
+
+    private static <T extends EventContext> AnalysisResult analyze(T eventContext, Function<T, ? extends CqnStructuredTypeRef> getCqnRef) {
+        return createAnalyzer(eventContext).analyze(getCqnRef.apply(eventContext));
+    }
+
+    private static <T extends EventContext> CqnAnalyzer createAnalyzer(T eventContext) {
+        return CqnAnalyzer.create(eventContext.getModel());
+    }
+}
